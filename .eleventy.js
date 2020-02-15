8095dc9e34ca83024ed6bbe6e1e348980927afc7
armand <armand@macbook-pro-darmand.home>
2020-02-15 18:58:33 +0100

    add barbajs transition and remove slick beacause of issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,35 @@
-const c = require("@contentful/rich-text-html-renderer");
+const { documentToHtmlString } = require("@contentful/rich-text-html-renderer");
+const localImages = require("eleventy-plugin-local-images");
 
 module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy("src/_assets");
 
   eleventyConfig.addShortcode("getHtml", data => {
-    return c.documentToHtmlString(data);
+    return documentToHtmlString(data);
   });
+
+  eleventyConfig.addPlugin(localImages, {
+    distPath: "dist",
+    assetPath: "/_assets/img"
+  });
+
+  // eleventyConfig.addShortcode("imgBase64", data => {
+  //   axios
+  //     .get("http:" + data)
+  //     .then(response => {
+  //       let returnedB64 = Buffer.from(response.data).toString("base64");
+  //       return response;
+  //     })
+  //     .catch(err => console.log(err));
+
+  //   // image2base64(`https:${data}`)
+  //   //   .then(response => {
+  //   //     return response;
+  //   //   })
+  //   //   .catch(error => {
+  //   //     console.log(error); //Fail quietly....
+  //   //   });
+  // });
 
   return {
     passthroughFileCopy: true,
