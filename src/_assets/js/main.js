4a338dcba31129f81390f634c18a40832b78a49b
armand <armand@macbook-pro-darmand.home>
2020-02-15 22:13:36 +0100

    slick + page transitions ok

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -1,128 +1,90 @@
-// $(".slider").slick({
-//   dots: false,
-//   infinite: true,
-//   arrows: false,
-//   speed: 300,
-//   autoplay: true,
-//   slidesToShow: 3,
-//   responsive: [
-//     {
-//       breakpoint: 1240,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 1,
-//         dots: true
-//       }
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         dots: true
-//       }
-//     }
-//   ]
-// });
+const initSliders = () => {
+  $(".slider").slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1240,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+    ]
+  });
 
-// $(".slider-avis").slick({
-//   infinite: true,
-//   speed: 500,
-//   autoplay: true,
-//   adaptiveHeight: false,
-//   adaptiveWidth: false,
-//   responsive: [
-//     {
-//       breakpoint: 900,
-//       settings: {
-//         dots: true,
-//         arrows: false
-//       }
-//     }
-//   ]
-// });
+  $(".slider-avis").slick({
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    adaptiveHeight: false,
+    adaptiveWidth: false,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+};
 
-// barba.hooks.enter(data => {
-//   $(".slider").slick({
-//     dots: false,
-//     infinite: true,
-//     arrows: false,
-//     speed: 300,
-//     autoplay: true,
-//     slidesToShow: 3,
-//     responsive: [
-//       {
-//         breakpoint: 1240,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 1,
-//           dots: true
-//         }
-//       },
-//       {
-//         breakpoint: 600,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-//           dots: true
-//         }
-//       }
-//     ]
-//   });
+initSliders();
 
-//   $(".slider-avis").slick({
-//     infinite: true,
-//     speed: 500,
-//     autoplay: true,
-//     adaptiveHeight: false,
-//     adaptiveWidth: false,
-//     responsive: [
-//       {
-//         breakpoint: 900,
-//         settings: {
-//           dots: true,
-//           arrows: false
-//         }
-//       }
-//     ]
-//   });
-// });
+const toggles = document.querySelectorAll(".toggle__link");
 
-barba.use(barbaPrefetch);
+const toggle = e => {
+  e.preventDefault();
+  toggles.forEach(link => {
+    link.classList.remove("active");
+  });
+  e.target.classList.add("active");
+  document.querySelectorAll(".toggle__item").forEach(item => {
+    item.classList.add("d-none");
+  });
+  document.querySelector(e.target.hash).classList.remove("d-none");
+};
+
+toggles.forEach(el => el.addEventListener("click", e => toggle(e)));
+
+$(".btn-decouvrir").on("click", function() {
+  var page = $(this).attr("href");
+  var speed = 750;
+  $("html, body").animate({ scrollTop: $(page).offset().top }, speed);
+  return false;
+});
+
+const bodyTag = document.querySelector("body");
 
 barba.init({
-  debug: true,
   transitions: [
     {
-      name: "switch",
-      once({ current, next, trigger }) {
-        return new Promise(resolve => {
-          const images = document.querySelectorAll("img");
-
-          gsap.set(next.container, { opacity: 0 });
-
-          imagesLoaded(images, () => {
-            const tl = gsap.timeline({
-              onComplete() {
-                resolve();
-              }
-            });
-
-            tl.set(next.container, { opacity: 0 }).to(next.container, { opacity: 1, delay: 0.35 });
-          });
-        });
-      },
+      name: "fade",
       leave({ current, next, trigger }) {
         return new Promise(resolve => {
           const tl = gsap.timeline({
             onComplete() {
-              //window.scrollTo(0, 0);
               current.container.remove();
               resolve();
             }
           });
 
-          tl.to(current.container, { opacity: 0 });
+          tl.to(".links", { opacity: 0 }, 0).to(".section", { opacity: 0 }, 0.1);
         });
       },
       enter({ current, next, trigger }) {
@@ -132,8 +94,18 @@ barba.init({
               resolve();
             }
           });
+          initSliders();
 
-          tl.set(next.container, { opacity: 0 }).to(next.container, { opacity: 1 });
+          if (trigger.classList.contains("btn-contact")) {
+            var page = "#links";
+            var speed = 850;
+            $("html, body").animate({ scrollTop: $(page).offset().top }, speed);
+            return false;
+          }
+
+          tl.from(".links", { opacity: 0 }, 0)
+            .from(".section", { opacity: 0 }, 0)
+            .to(".section", { opacity: 1 }, 0.1);
         });
       }
     }
